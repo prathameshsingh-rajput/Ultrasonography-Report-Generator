4ff3c57113e3a83dfb78b8b94af6822705fc0987
rajput_prathameshsigh <rprathameshsingh@gmail.com>
2024-07-31 16:05:43 +0530

    Adding Login Panel

--- src/main/java/org/prathameshsingh/MainFrame.java
+++ src/main/java/org/prathameshsingh/MainFrame.java
@@ -2,22 +2,19 @@ package org.prathameshsingh;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
     private JLabel hospitalName;
     private JButton minorReportButton;
     private JButton majorReportButton;
     private JButton photoReportButton;
-    private JLabel  allRightsReserve ;
-
+    private JLabel allRightsReserve;
 
     Font buttonFont = new Font("Roboto", Font.BOLD, 20);
     Font componeyNameFont = new Font("Roboto", Font.BOLD, 40);
+    Font robotoBold = new Font("Roboto", Font.BOLD, 26);
+
 //    Font rightReserveFont = new Font("Roboto", Font.PLAIN, 11);
-
-
 
     public MainFrame() {
         setLayout(null);
@@ -33,6 +30,17 @@ public class MainFrame extends JFrame {
 
         setResizable(false);
 
+        // Initially set the content pane to the login panel
+        setContentPane(new LoginPanel(this));
+
+        // Custom frame icon
+        ImageIcon icon = new ImageIcon("C:/Users/hp22r/OneDrive/Desktop/Hospital Project/PsrSoftware/src/main/resources/appIcon.png"); // Replace with the path to your icon file
+        setIconImage(icon.getImage());
+    }
+
+    public void showMainPanel() {
+        getContentPane().removeAll();
+        setLayout(null);
 
         hospitalName = new JLabel("PSR-SoftHub");
         hospitalName.setFont(componeyNameFont);
@@ -41,24 +49,27 @@ public class MainFrame extends JFrame {
         add(hospitalName);
 
         minorReportButton = new JButton("Minor Report");
-        minorReportButton.setFont(buttonFont);
-        minorReportButton.setBackground(UIManager.getColor("Button.background"));
-        minorReportButton.setForeground(UIManager.getColor("Label.foreground"));
-        minorReportButton.setBounds(240, 450, 250,60);
+        minorReportButton.setFont(robotoBold);
+        minorReportButton.setBackground(new Color(30, 144, 255));
+        minorReportButton.setForeground(Color.WHITE);
+        minorReportButton.setBorderPainted(false);
+        minorReportButton.setBounds(240, 450, 250, 60);
         add(minorReportButton);
 
         majorReportButton = new JButton("Major Report");
-        majorReportButton.setFont(buttonFont);
-        majorReportButton.setBackground(UIManager.getColor("Button.background"));
-        majorReportButton.setForeground(UIManager.getColor("Label.foreground"));
-        majorReportButton.setBounds(600, 450, 250,60);
+        majorReportButton.setFont(robotoBold);
+        majorReportButton.setBackground(new Color(30, 144, 255));
+        majorReportButton.setForeground(Color.WHITE);
+        majorReportButton.setBorderPainted(false);
+        majorReportButton.setBounds(600, 450, 250, 60);
         add(majorReportButton);
 
         photoReportButton = new JButton("Photo Report");
-        photoReportButton.setFont(buttonFont);
-        photoReportButton.setBackground(UIManager.getColor("Button.background"));
-        photoReportButton.setForeground(UIManager.getColor("Label.foreground"));
-        photoReportButton.setBounds(975, 450, 250,60);
+        photoReportButton.setFont(robotoBold);
+        photoReportButton.setBackground(new Color(30, 144, 255));
+        photoReportButton.setForeground(Color.WHITE);
+        photoReportButton.setBorderPainted(false);
+        photoReportButton.setBounds(975, 450, 250, 60);
         add(photoReportButton);
 
 //        allRightsReserve = new JLabel("© 2024 Prathamesh Singh Rajput [PSR]. All rights reserved.");
@@ -67,51 +78,35 @@ public class MainFrame extends JFrame {
 //        allRightsReserve.setBounds(470, 520, 700, 30);
 //        add(allRightsReserve);
 
-        minorReportButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                MinorReportMainPanel minorReportMainPanel = new MinorReportMainPanel();
-                setContentPane(minorReportMainPanel);  // Set the content pane to the new MainPanel
-                validate();  // Validate the new content pane
-                repaint();   // Repaint the frame
-            }
+        minorReportButton.addActionListener(e -> {
+            MinorReportMainPanel minorReportMainPanel = new MinorReportMainPanel();
+            setContentPane(minorReportMainPanel);  // Set the content pane to the new MainPanel
+            validate();  // Validate the new content pane
+            repaint();   // Repaint the frame
         });
 
-        majorReportButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                MajorReportMainPanel majorReportMainPanel = new MajorReportMainPanel();
-                setContentPane(majorReportMainPanel);
-                validate();
-                repaint();
-            }
+        majorReportButton.addActionListener(e -> {
+            MajorReportMainPanel majorReportMainPanel = new MajorReportMainPanel();
+            setContentPane(majorReportMainPanel);
+            validate();
+            repaint();
         });
 
-        photoReportButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                PhotoReportMainPanel photoReportMainPanel = new PhotoReportMainPanel();
-                setContentPane(photoReportMainPanel);
-                validate();
-                repaint();
-            }
+        photoReportButton.addActionListener(e -> {
+            PhotoReportMainPanel photoReportMainPanel = new PhotoReportMainPanel();
+            setContentPane(photoReportMainPanel);
+            validate();
+            repaint();
         });
 
-        // Custom frame icon
-        ImageIcon icon = new ImageIcon("C:/Users/hp22r/OneDrive/Desktop/Hospital Project/PsrSoftware/src/main/resources/appIcon.png"); // Replace with the path to your icon file
-        setIconImage(icon.getImage());
+        validate();
+        repaint();
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                MainFrame mainFrame = new MainFrame();
-                mainFrame.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
         });
     }
-
-
 }
-
